c9c95f81a1c154e9e0e9a660ffebd678ca6b5b2d
HP <HP@LAPTOP-34BSFIJF>
2024-03-08 18:11:24 +0530

    Added m4 method from IDE

--- Demo1/src/Dem/A.java
+++ Demo1/src/Dem/A.java
@@ -1,18 +1,20 @@
 package Dem;
 
 public class A {
-	
 
-  public void m1()
-  {
-  }
-  
-  public static void main(String[] args) {
-	System.out.println("Welcome");
-}
-	public void m3()
-	{
+	public void m1() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+	}
+
+	public void m3() {
 		System.out.println("This is m3");
+	}
+
+	public void m4() {
+		System.out.println("This is m4");
 	}
 
 }
